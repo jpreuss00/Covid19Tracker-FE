442f3e6d0d6bdcdbc38ab04d228a5a5410e2f114
Preuß <joshua.preuss2000@gmx.de>
2020-04-27 11:02:35 +0200

    added deletefunction

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
     $(".register__userButton").click(handleRegister);
+    $(".delete__userButton").click(handleDelete);
 
 });
 
@@ -9,20 +10,42 @@ function handleRegister(){
 }
 
 function handleRegisterRequest(callback){
-    data = $.get({
-        url: "https://enigmatic-retreat-23877.herokuapp.com/register?registration=true",
-        success: function (data) {
-                    callback(data);
-                },
-        statusCode: {
-            500: function () {
-                console.log("error");
-            }
-        }
-    });
-}
+     data = $.get({
+         url: "https://enigmatic-retreat-23877.herokuapp.com/register",
+         success: function (data) {
+                     callback(data);
+                 },
+         statusCode: {
+             500: function () {
+                 console.log("error");
+             }
+         }
+     });
+ }
 
 function showUserData(data){
     $(".register__userID").val(data.id);
     $(".register__deletecode").val(data.deletecode);
+}
+
+function handleDelete(){
+    deleteCode = $(".delete_deleteCode").val();
+    handleDeleteRequest(deleteCode);
+}
+
+function handleDeleteRequest(deleteCode){
+    data = $.get({
+        url: "https://enigmatic-retreat-23877.herokuapp.com/delete?deleteCode="+deleteCode,
+        statusCode: {
+            400: function () {
+                console.log("error");
+            },
+            404: function () {
+                console.log("error");
+            },
+            204: function () {
+                console.log("error");
+            }
+        }
+    });
 }
